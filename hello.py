b2cd94505ca5aa3c037b4cc651056b12dd096fd8
shingyu <shyu@staredges.cn>
2024-07-25 03:07:22 +0800

    test

---
+++ hello.py
@@ -0,0 +1,50 @@
+# # print("hello world! 你好 世界!")
+#
+# # 1.导入系统的 keyword 库/模块
+# import keyword
+#
+# # 2.输出查看系统的关键字列表
+# print(keyword.kwlist)
+#
+# # 3.输出查看系统的关键字列表的总长度
+# print(len(keyword.kwlist))
+
+
+#定义字符串变量 name ，输出 我的名字叫 小明，请多多关照！
+name = '小明'
+print(f'我的名字叫{name}，请多多关照！')
+
+#定义字符串变量 name ，age，输出 我的名字叫 小明，今年 18 岁，请多多关照！
+name = '小明'
+age = 18
+print(f'我的名字叫{name}，今年{age}岁，请多多关照！')
+print('我的名字叫%s，今年%d岁，请多多关照！' % (name, age))
+
+#定义整数变量student_no ，输出我的学号是000001
+student_no = 1
+print('我的学号是%06d' % student_no)
+#定义小数price、weight、money，输出苹果单价9.00元/斤，购买了5.00斤，需要支付45.00元
+price = 9.00
+weight = 5.00
+money = price * weight
+print(f'苹果单价%0.2f元/斤，购买了%0.2f斤，需要支付%0.2f' % (price, weight, money))
+
+#定义一个小数scale，输出 数据比列是10.00%
+scale = 10.00
+print('数据比列是%0.2f%%' % scale)
+
+
+'''
+啊啊飒飒东风风光过后
+'''
+
+'''
+\     sds \\
+YHVVJHGHVGGHHGB
+'''
+
+
+
+
+
+
